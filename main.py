055f2bbb90fb0ae20dda89f8f9e2475407c69699
DergSnippy <symen.postma@hotmail.com>
2025-07-02 21:01:51 +0200

    Finished bookbot!

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from stats import word_count, char_count
+import sys
+from stats import word_count, char_count, sorted_char_count
 
 def get_book_text(book_path):
     with open(book_path) as file:
         return file.read()
     
 def main():
-    book_path = 'books/frankenstein.txt' 
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f" Analyzing book found at {book_path}...")  
     book_text = get_book_text(book_path)
     word_count(book_text)
-    char_count(book_text)
+    sorted_char_count(book_text)
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,10 @@
 def word_count(text):
     words = text.split()
-    print(f"{len(words)} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {len(words)} total words")
 
 def char_count(text):
+    print("----------- Character Count ----------")
     low_text = text.lower()
     char_dict = {}
     for char in low_text:
@@ -11,4 +13,17 @@ def char_count(text):
         else:
             char_dict[char] = 1
     for char in char_dict:
-        print(f"''{char}': {char_dict[char]}'")
+        print(f"{char}: {char_dict[char]}")
+
+def sorted_char_count(text):
+    print("----------- Character Count ----------")
+    low_text = text.lower()
+    char_dict = {}
+    for char in low_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    sorted_chars = sorted(char_dict.items(), key=lambda item: item[0])
+    for char, count in sorted_chars:
+        print(f"{char}: {count}")
